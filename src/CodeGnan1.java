e89b42feb78732aa58377a9be2e0f75790d1746d
dv <venusd95@gmail.com>
2023-04-25 14:59:40 +0530

    second day

--- src/CodeGnan1.java
+++ src/CodeGnan1.java
@@ -9,6 +9,7 @@ class CodeGnan1
 	static String b="java";
 public static void main(String[] args) 
 {
+//creation of object 
 	CodeGnan1 c = new CodeGnan1();
 	double d=12.67;
     System.out.println(c.a);
